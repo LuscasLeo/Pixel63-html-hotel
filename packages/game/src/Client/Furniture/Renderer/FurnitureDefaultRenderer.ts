8dc8237c7728a7d02251b02b0a391c5c9de40003
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-07-18 19:54:18 +0200

    fix: furniture render to canvas blend modes

--- packages/game/src/Client/Furniture/Renderer/FurnitureDefaultRenderer.ts
+++ packages/game/src/Client/Furniture/Renderer/FurnitureDefaultRenderer.ts
@@ -463,7 +463,7 @@ export default class FurnitureDefaultRenderer implements FurnitureRenderer {
             context.save();
 
             if(sprite.ink) {
-                //context.globalCompositeOperation = sprite.ink;
+                context.globalCompositeOperation = (sprite.ink === "add")?("lighten"):(sprite.ink as GlobalCompositeOperation);
             }
 
             if(sprite.alpha) {
