38ca310b1dd0e651bd0015cc7694ea6014a47734
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-07-20 16:19:54 +0200

    chore: center room camera

--- packages/game/src/Client/Room/RoomRenderer.ts
+++ packages/game/src/Client/Room/RoomRenderer.ts
@@ -118,7 +118,7 @@ export default class RoomRenderer extends EventTarget {
         }
 
         this.camera.cameraPosition.left = Math.round(this.application.screen.width / 2);
-        this.camera.cameraPosition.left -= (this.structure.grid.length) * 6;
+        this.camera.cameraPosition.left -= (this.structure.grid.length ) * 3;
         
         this.camera.cameraPosition.top = Math.round(this.application.screen.height / 2);
         this.camera.cameraPosition.top -= (this.structure.grid.length + this.structure.grid[0]?.length) * 6;
